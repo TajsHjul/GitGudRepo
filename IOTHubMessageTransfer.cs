bb58f2eb7bf76cb8d60e7a7a49778e0e1db9abff
Rulz123 <75121436+Rulz123@users.noreply.github.com>
2022-05-11 17:14:30 +0200

    Added IOTHubmessage class
    Added class to send messages from raspberry pi to IOT hub needs testing

---
+++ IOTHubMessageTransfer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+
+namespace RasbPiProject
+{
+    class IOTHubMessageTransfer
+    {
+        private static readonly string connectionString = "[Connnection String]";
+        private static DeviceClient deviceClient;
+
+        static void MessageSender()
+        {
+            deviceClient = DeviceClient.CreateFromConnectionString(connectionString);
+
+            ReceiveCloudToDeviceMessageAsync();
+            SendDeviceToCloudMessageAsync();
+
+
+        }
+
+        private static async void SendDeviceToCloudMessageAsync()
+        {
+            var messageString = "Random test message";
+            Message message = new Message(Encoding.ASCII.GetBytes(messageString));            
+
+            await deviceClient.SendEventAsync(message);
+            Console.WriteLine("Sending Message {0}", messageString);
+
+        }
+
+        private static async void ReceiveCloudToDeviceMessageAsync()
+        {
+            Console.WriteLine("Receiving Cloud to Device messages from IoT Hub");
+
+            while (true)
+            {
+                Message receivedMessage = await deviceClient.ReceiveAsync();
+
+                if (receivedMessage != null)
+                {
+                    string receivedMessageString = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+                    Console.WriteLine("Received message: {0}", receivedMessageString);
+                    await deviceClient.CompleteAsync(receivedMessage);
+                }
+
+            }
+        }
+    }
+}
